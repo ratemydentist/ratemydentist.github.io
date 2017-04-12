3d1de0c1852edb65e7e257893eed4ad57e25982b
nefreez <nefreez@gmail.com>
2017-04-12 19:40:26 +0100

    Update book.php

--- book.php
+++ book.php
@@ -1,24 +1,7 @@
-<!DOCTYPE html>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<html>
-<head>
-BOOKING PAGE - NO BOOTSTRAP - NO THEME - NO JAVA <br>
-</head>
-<body>
-    
-<?php
-echo "PHP is working";
+<?php      
+    include 'template_part_a.php';
 ?>
-    
-    
-<div id="main" style="background-color:red;position:relative;">
+<div id="main" style="background-color:red;position:relative;color:black;">
     <form action="./book_processing.php" method="post" style="border-style:solid;position:relative;" >
         <div id="booking_info" style="float:right;border-style:solid;width:40%;position:relative;">
             <input type="hidden" readonly name="time_plan"      value="<?php echo"TIME PARAMETER"; ?>"><br>
@@ -45,7 +28,6 @@ echo "PHP is working";
     </form>
     
 </div>
-    
-    
-</body>
-</html>
+<?php
+    include 'template_part_b.php';
+?>
