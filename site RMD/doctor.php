d965dba78ce8666825ac223799d10112e0651ce3
Nadin Puchnina <nadinpuchnina@gmail.com>
2017-04-13 20:19:31 +0300

    Connecting PHP files

---
+++ site RMD/doctor.php
@@ -0,0 +1,126 @@
+<?php include("header_RMD.php"); ?>
+     <!--content_r-->
+ <div class="container">
+     <div class="content_r col-xs-12 col-sm-12 col-md-12 col-lg-12">
+       
+       <div class="row">
+          <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 ">
+            <a href="#">
+              <img class="img-responsive" src="img/504.jpg_frame%20(15).jpg" alt="photo doctor">
+            </a>
+          </div>
+          
+          <div class="col-xs-12 col-sm-10 col-md-10 col-lg-10 ">
+           <!--name doctor-->
+            <a href="#"><h4 class="">Ksenia Ovchinnikova</h4></a>
+            <!--Clinic-->
+                 <div class="dropdown dr_clinic">
+                      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        Clinic
+                        <span class="caret"></span>
+                      </button>
+                      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                        <li><a href="#">Clinic1</a></li>
+                        <li><a href="#">Clinic2</a></li>
+                        <li><a href="#">Clinic3</a></li>
+                        <li><a href="#">Clinic4</a></li>
+                      </ul>
+                 </div>
+                 <!-- calendar -->
+                 <div class="panel panel-default ">
+                      <!-- Default panel contents calendar -->
+                      <div class="panel-heading"><span class="glyphicon glyphicon-calendar" aria-hidden="true"> </span> Calendar</div>
+                                                   <!-- Table -->
+                      <div class="table-responsive">
+                          <table class="table ser_pac">
+                            <tr>
+                               Fri
+                                <th><span class="glyphicon glyphicon-menu-left" aria-hidden="true"> </span></th>
+                                <th>Mon. May 15</th>
+                                <th>Tue. May 16</th>
+                                <th>Wed. May 17</th>
+                                <th>Thu. May 18</th>
+                                <th>Fri. May 19</th>
+                                <th>Sat. May 20</th>
+                                <th>Sun. May 21</th>
+                                <th><span class="glyphicon glyphicon-menu-right" aria-hidden="true"> </span></th>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>choose</td>
+                                <td>choose</td>
+                                <td>-</td>
+                                <td></td>
+                            </tr>
+                            
+                          </table>
+                      </div> 
+                 </div>
+                 <!-- book -->
+                 <div class=" bt_s">
+                        <a href="search_r.html">Book</a>
+                 </div>  
+                 <!-- Service packages -->
+                 <div class="panel panel-default ">
+                      <!-- Default panel contents Service packages -->
+                      <div class="panel-heading"><span class="glyphicon glyphicon-th-list" aria-hidden="true"> </span> Service packages</div>
+                                                   <!-- Table -->
+                      <div class="table-responsive">
+                          <table class="table ser_pac">
+                            <tr>
+                                <th>Package of services<span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></th>
+                                <th>Clinic<span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></th>
+                                <th><span class="glyphicon glyphicon-euro" aria-hidden="true"></span></th>
+                            </tr>
+                            <tr>
+                                <td>Package №1</td>
+                                <td>Result table</td>
+                                <td>Result table</td>
+                            </tr>
+                            <tr>
+                                <td>Package №2</td>
+                                <td>Result table</td>
+                                <td>Result table</td>
+                            </tr>
+                            <tr>
+                                <td>Package №3</td>
+                                <td>Result table</td>
+                                <td>Result table</td>
+                            </tr>
+                          </table>
+                      </div> 
+                 </div>
+                 <!-- Comments -->
+                 <div class="panel panel-default">
+                      <div class="panel-heading">
+                        <h3 class="panel-title"><span class="glyphicon glyphicon-star" aria-hidden="true"> </span> Comment1</h3>
+                      </div>
+                      <div class="panel-body">
+                        sldvsnkjvnslndvlsndlkvnslnvlksnldnvlsnvlnvl
+                      </div>
+                 </div>
+                 <!-- Comments -->
+                 <div class="panel panel-default">
+                      <div class="panel-heading">
+                        <h3 class="panel-title"><span class="glyphicon glyphicon-star" aria-hidden="true"> </span> Comment2</h3>
+                      </div>
+                      <div class="panel-body">
+                        sldvsnkjvnslndvlsndlkvnslnvlksnldnvlsnvlnvl
+                      </div>
+                 </div>
+                 <p></p>
+                 <br>
+                 <br>
+          </div>
+           </div>
+     </div>
+ </div>
+                 <br>
+                 <br>
+   <!--end content_r-->
+    <!--footer -->
+<?php include("footer_RMD.php"); ?>
