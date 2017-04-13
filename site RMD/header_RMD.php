c6d4ea75e25ea9700a9bedd8696a7576fa58d447
Nadin Puchnina <nadinpuchnina@gmail.com>
2017-04-13 20:00:43 +0300

    Separate files header, footer.
    Separate files header, footer. Connect both files to index

---
+++ site RMD/header_RMD.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Rate my dentist</title>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/jquery-3.2.0.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="rmd_nadin.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+
+    <!--memu-->
+    <!--<div class="container">
+
+      <!-- Static navbar -->
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav col-lg-9 col-md-8 col-sm-8">
+              <a class="col-xs-12 col-sm-2 col-md-2 col-lg-1" href="index.html">Home</a>
+              <a class="col-xs-12 col-sm-2 col-md-2 col-lg-1" href="*.html">Doctors</a>
+              <a class="col-xs-12 col-sm-2 col-md-2 col-lg-1" href="*.html">Clinics</a>
+              <a class="col-xs-12 col-sm-2 col-md-2 col-lg-1" href="*.html">Services</a>
+            </ul>
+            <ul class="nav navbar-nav navbar-right col-lg-3 col-md-4 col-sm-4">
+              <a class="col-xs-12 col-sm-3 col-md-3 col-lg-3" href="*.html">Log in</a>
+              <a class="col-xs-12 col-sm-3 col-md-3 col-lg-3" href="*.html">Sin up</a>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+  <!--  </div> <!-- /container -->
+     <!--end memu-->
