26426b45ca28964d0a3df04b7576f2dac99fb4f8
Nadin Puchnina <nadinpuchnina@gmail.com>
2017-04-13 20:01:39 +0300

    Separate files header, footer.
    Separate files header, footer. Connect both files to index

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php include("header_RMD.php"); ?>
+    <!--logo-->
+    <div class="container">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><p class="text-center logo_rmd">Rate my Dentist</p></div>
+    </div>
+    <!--endlogo-->
+    <!-- search -->
+    <div class="container search_p">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dr1">
+            <div class="dropdown">
+                  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    Specialization Dantist
+                    <span class="caret"></span>
+                  </button>
+                  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <li><a href="#">Diagnosis of teeth</a></li>
+                    <li><a href="#">Correction of occlusion</a></li>
+                    <li><a href="#">Dental treatment</a></li>
+                    <li><a href="#">Implantation of teeth</a></li>
+                    <li><a href="#">Children's dentistry</a></li>
+                    <li><a href="#">Tooth whitening</a></li>
+                    <li><a href="#">Periodontology</a></li>
+                    <li><a href="#">Prosthetic dentistry</a></li>
+                    <li><a href="#">Surgical stomatology</a></li>
+                    <li><a href="#">Aesthetic dentistry</a></li>
+                  </ul>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dr2">
+            <div class="dropdown">
+                  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    City
+                    <span class="caret"></span>
+                  </button>
+                  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                    <li><a href="#">Tallinn</a></li>
+                    <li><a href="#">Tartu</a></li>
+                    <li><a href="#">Narva</a></li>
+                    <li><a href="#">Pärnu</a></li>
+                  </ul>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bt_s">
+            <a href="search_r.html">Search</a>
+        </div>                
+    </div>
+   <!-- end search -->
+   <!--footer-->
+<?php include("footer_RMD.php"); ?>
