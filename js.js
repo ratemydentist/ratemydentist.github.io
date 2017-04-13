7de309eb45b2c9de29e6fab0b9f98403d03e170d
huseyinkomac <hkoma7@gmail.com>
2017-04-13 13:57:50 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 var username = "Jeremy";
 var password = "Smith";
-
+var is_checked = false;
              
 function myFunction() {
 	if(document.getElementById("username_input").value == username){
@@ -9,4 +9,18 @@ function myFunction() {
 		}
 	} else 
 		alert('You entered wrong username/password.');
+}
+
+
+function appFunction(){
+	var counter = document.getElementsByTagName('input');
+  for (var i=0; i<counter.length; i++) {
+    if (counter[i].type == 'radio' && counter[i].checked) {
+      is_checked=true;
+      alert('you picked one.');
+    }
+   }
+      if (is_checked == false) {
+      alert('You did not choose any time.');
+    }
 }
