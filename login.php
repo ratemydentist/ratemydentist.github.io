eab4a00885550673ea3e0d4b5413943d12e72b79
nefreez <nefreez@gmail.com>
2017-04-21 00:33:21 +0300

    Update login.php

--- login.php
+++ login.php
@@ -6,14 +6,18 @@
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><p class="text-center logo_rmd">Login page</p></div>
     </div>
     <!--endlogo-->
-
-    <script src="./js/js.js" ></script>
-
-    <div id="main_login" style="background-color:red;position:relative; color:black;">
-            <input type="text" id="username_input" value="Login"><br>
-            <input type="text" id="password_input" value="Password"><br>
-            <input type="button" id="login" value="LOGIN" onclick="myFunction()"><br> <!-- JS does the login check-->
-    </div>
+    <br><br><br>
+    <div class="row">
+       <div class="col-md-4"></div>
+       <div class="col-md-4">
+          <div id="main_login" style="color:black;margin:0 auto;width:50%;">
+             <input type="text" id="username_input" placeholder="Login" style="width:100%;text-align:center;"><br><br>
+             <input type="text" id="password_input" placeholder="Password" style="width:100%;text-align:center;"><br><br>
+             <input type="button" id="login" value="LOGIN" onclick="myFunction()" style="width:100%;text-align:center;"><br> <!-- JS does the login check-->
+          </div>
+       </div>
+       <div class="col-md-4"></div>
+    </div> <!-- end row -->
 <?php
      include 'template_part_b.php';
 ?>
