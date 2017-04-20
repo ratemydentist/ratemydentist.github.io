53d0f0062d227889cedc2cc795ddd710095fefd0
nefreez <nefreez@gmail.com>
2017-04-21 00:48:20 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,22 +1,31 @@
 <?php      
     include 'template_part_a.php';
 ?>
-<div id="main_register" style="background-color:red;position:relative;color:black;">
-    <form action="./do_register.php" method="post">
-            <input type="text" name="login_input" value="New login"><br>
-            <input type="text" name="password_input" value="New password"><br>
-            <input type="text" name="email_input" value="your email"><br>
-            <input type="text" name="name_input" value="your given name"><br>
-            <input type="text" name="familyname_input" value="your familyname"><br>
-            <input type="text" name="idcode_input" value="your id code"><br>
-            <input type="text" name="phonenumber_input" value="(+372)your phone number"><br>
-            
-            <input type="submit" name="login" value="REGISTER"><br>
-
-    </form>
-    
-</div>
+    <!--logo-->
+    <div class="container">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><p class="text-center logo_rmd">Register</p></div>
+    </div>
+    <!--endlogo-->
+    <br>
+    <div class="row">
+       <div class="col-md-4"></div>
+       <div class="col-md-4">
+          <div id="main_register" style="width:50%;margin:0 auto;color:black;">
+             <form action="./do_register.php" method="post">
+                <input type="text" name="login_input" style="width:100%;" placeholder="New login"><br><br>
+                <input type="text" name="password_input" style="width:100%;" placeholder="New password"><br><br>
+                <input type="text" name="email_input" style="width:100%;" placeholder="your email"><br><br>
+                <input type="text" name="name_input" style="width:100%;" placeholder="your given name"><br><br>
+                <input type="text" name="familyname_input" style="width:100%;" placeholder="your familyname"><br><br>
+                <input type="text" name="idcode_input" style="width:100%;" placeholder="your id code"><br><br>
+                <input type="text" name="phonenumber_input" style="width:100%;" placeholder="(+372)your phone number"><br><br>
+                <input type="submit" name="login" style="width:100%;" value="REGISTER">
+             </form>
+          </div>
+       </div>
+       <div class="col-md-4"></div>
   
 <?php
      include 'template_part_b.php';
 ?>
+
