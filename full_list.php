03a39255e3f92ced9a10bb6158bd5505e237637c
Nadin Puchnina <nadinpuchnina@gmail.com>
2017-04-21 16:35:23 +0300

    new page
    full list all doctors

---
+++ full_list.php
@@ -0,0 +1,39 @@
+<?php include("template_part_a.php"); ?>
+    <!--list-->
+       <!--content_r-->
+ <div class="container">
+     <div class="content_r col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <div class="table-responsive">
+                           <table class="table">
+                              <tr>
+                                 <th>Doctor<span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></th>
+                                 <th>Clinic<span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></th>
+                                  <th><span class="glyphicon glyphicon-euro" aria-hidden="true"></span><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></th>
+                                  <th><span class="glyphicon glyphicon-star" aria-hidden="true"></span><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></th>
+                              </tr>
+                              <tr>
+                                 <td>Doctor name</td>
+                                 <td>Clinic name</td>
+                                 <td>100</td>
+                                 <td>Result table</td>
+                              </tr>
+                              <tr>
+                                 <td>Doctor name</td>
+                                 <td>Clinic name</td>
+                                 <td>80</td>
+                                 <td>Result table</td>
+                              </tr>
+                              <tr>
+                                 <td>Doctor name</td>
+                                 <td>Clinic name</td>
+                                 <td>4</td>
+                                 <td>5</td>
+                              </tr>
+                           </table>
+                        </div> 
+                    </div> <!-- end table -->
+     </div>
+ </div>
+ 
+   <!--footer-->
+<?php include("template_part_b.php"); ?>
