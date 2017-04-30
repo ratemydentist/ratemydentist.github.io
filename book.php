4e41b2f0f0de9e43efbbd3d3cd17ad6249c873e9
nefreez <nefreez@gmail.com>
2017-04-30 15:33:48 +0300

    Update book.php

--- book.php
+++ book.php
@@ -1,30 +1,50 @@
 <?php      
     include 'template_part_a.php';
+    session_start();
+
 ?>
-<div id="main" style="background-color:red;position:relative;color:black;">
-    <form action="./book_processing.php" method="post" style="border-style:solid;position:relative;" >
-        <div id="booking_info" style="float:right;border-style:solid;width:40%;position:relative;">
-            <input type="hidden" readonly name="time_plan"      value="<?php echo"TIME PARAMETER"; ?>"><br>
-            <input type="hidden" readonly name="date_plan"      value="<?php echo"DATE PARAMETER"; ?>"><br>
-            <input type="hidden" readonly name="doctor_plan"    value="<?php echo"PHONE PARAMETER"; ?>"><br>
-            <input type="hidden" readonly name="address_plan"   value="<?php echo"REASON PARAMETER"; ?>"><br>
-            
-            <div id="time_plan">    VISIT TIME: <?php echo"TIME PARAMETER"; ?> </div>
-            <div id="date_plan">    DATE:       <?php echo"DATE PARAMETER"; ?></div>
-            <div id="doctor_plan">  DOCTOR:     <?php echo"PHONE PARAMETER"; ?></div>
-            <div id="address_plan"> ADDRESS:    <?php echo"REASON PARAMETER"; ?></div>
+<?php
+      $day = "unspecified";
+      $month = "unspecified";
+      $hour = "unspecified";
+      $minute = "unspecified";
+   if (isset($_POST["datetime"]) && !empty($_POST["datetime"])){
+      $when = $_POST["datetime"];
+      $day = substr($when,0,2);
+      $month = "May 2017";
+      $hour = substr($when,2,2);
+      $minute = substr($when,4,6);
+      $clinic = $_POST["clinic_sel"];
+   }
+
+?>
+<div id="main">
+    <form action="./book_processing.php" method="post">
+      <div class="row" style="color:black;">
+        <div class="col-md-2 "></div>
+        <div class="col-md-4 test">
+            <input readonly name="time_plan" value="<?php echo "VISIT TIME: ".$hour.":".$minute; ?>"><br>
+            <input readonly name="date_plan" value="<?php echo "DATE: ".$day." ".$month; ?>"><br>
+            <input readonly name="doctor_plan" value="Ksenia Ovchinnikova"><br>
+            <textarea readonly name="address_plan" value="<?php echo "ADDRESS: ".$clinic; ?>"><?php echo "clinic: ".$clinic; ?></textarea><br>
         </div>
-        <div id="patient_info" style="float:left;border-style:solid;width:40%;position:relative;">
-            <input type="text" name="name_input"    value="<?php echo"PATIENT NAME PARAMETER"; ?>"><br>            
-            <input type="text" name="idcode_input"  value="<?php echo"IDCODE PARAMETER"; ?>"><br>
-            <input type="text" name="email_input"  value="<?php echo"EMAIL PARAMETER"; ?>"><br>
-            <input type="text" name="phone_input"   value="<?php echo"PHONE PARAMETER"; ?>"><br>
-            <input type="text" name="reason_input"  value="<?php echo"REASON "; ?>"><br>
+        <div class="col-md-4">
+            <input type="text" name="name_input" style="width:100%;text-align:center;" value="<?php echo $_SESSION["name"]." ".$_SESSION["fname"]; ?>"><br>            
+            <input type="text" name="idcode_input" style="width:100%;text-align:center;" 
+value="<?php echo $_SESSION["id"]; ?>"><br>
+            <input type="text" name="email_input" style="width:100%;text-align:center;" value="<?php echo $_SESSION["mail"]; ?>"><br>
+            <input type="text" name="phone_input" style="width:100%;text-align:center;" value="<?php echo $_SESSION["phone"]; ?>"><br>
+            <textarea name="reason_input" rows="5" style="width:100%;text-align:center;" placeholder="<?php echo"REASON FOR VISIT"; ?>"></textarea><br>
         </div>
-        <div style="float:left;border-style:solid;width:100%;position:relative;">
-            <input type="submit" name="cancel" value="CANCEL"><br>
+        <div class="col-md-2 "></div>
+      </div> <!-- end row --> 
+      <br><br>
+      <div class="row">
+        <div class="col-md-2 "></div>
+        <div class="col-md-10" style="color:black;">
             <input type="submit" name="book" value="BOOK"><br>
         </div>
+      </div> <!-- end row -->  
     </form>
     
 </div>
